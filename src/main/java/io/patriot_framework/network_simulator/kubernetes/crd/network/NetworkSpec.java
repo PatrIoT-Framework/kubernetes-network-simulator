09aa46c95b257b5638903eb27a5ae685a90838ef
Jakub <jakub.smadis@gmail.com>
2021-03-25 23:54:41 +0100

    Add disable inside traffic attributes to the network spec

--- src/main/java/io/patriot_framework/network_simulator/kubernetes/crd/network/NetworkSpec.java
+++ src/main/java/io/patriot_framework/network_simulator/kubernetes/crd/network/NetworkSpec.java
@@ -1,7 +1,27 @@
 package io.patriot_framework.network_simulator.kubernetes.crd.network;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.fabric8.kubernetes.api.model.KubernetesResource;
 
 public class NetworkSpec implements KubernetesResource {
+    @JsonProperty("disableInsideIngressTraffic")
+    private boolean disableInsideIngressTraffic;
+    @JsonProperty("disableInsideEgressTraffic")
+    private boolean disableInsideEgressTraffic;
 
+    public boolean isDisableInsideIngressTraffic() {
+        return disableInsideIngressTraffic;
+    }
+
+    public void setDisableInsideIngressTraffic(boolean disableInsideIngressTraffic) {
+        this.disableInsideIngressTraffic = disableInsideIngressTraffic;
+    }
+
+    public boolean isDisableInsideEgressTraffic() {
+        return disableInsideEgressTraffic;
+    }
+
+    public void setDisableInsideEgressTraffic(boolean disableInsideEgressTraffic) {
+        this.disableInsideEgressTraffic = disableInsideEgressTraffic;
+    }
 }
